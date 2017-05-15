36822e30a361867a24a4a205f9ba70a6c29cb61b
skywwh1 <skywwh1@126.com>
2017-05-15 17:54:20 +0800

    init

---
+++ admin/config/test.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'id' => 'app-admin-tests',
+    'components' => [
+        'assetManager' => [
+            'basePath' => __DIR__ . '/../web/assets',
+        ],
+    ],
+];
---
+++ advertiser/config/test.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'id' => 'app-advertiser-tests',
+    'components' => [
+        'assetManager' => [
+            'basePath' => __DIR__ . '/../web/assets',
+        ],
+    ],
+];
